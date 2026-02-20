db300db68796e98c7d45022e92f85335e3c9c164
ADMIN <siddharthghadge.skncoe.mech@gmail.com>
2026-02-20 11:00:39 +0530

    commit

--- src/main/java/com/carrental/paymentservice/client/BookingServiceClient.java
+++ src/main/java/com/carrental/paymentservice/client/BookingServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "bookingservice",
-        url = "${BOOKING.SERVICE.URL}"
+        url = "${booking.service.url}"
 )
 public interface BookingServiceClient {
 
